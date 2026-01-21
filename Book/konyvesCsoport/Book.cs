ae86e5a0c9de2d7621a4ff7a10b59bc1f180a627
9esTerem <9esTerem@ktch.hu>
2026-01-21 19:24:17 +0100

    book class_2

--- Book/konyvesCsoport/Book.cs
+++ Book/konyvesCsoport/Book.cs
@@ -8,7 +8,12 @@ namespace konyvesCsoport
 {
     internal class Book
     {
+        public  string ISBN { get; set; }
         public string Title { get; set; }
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+        public bool IsAvailable { get; set; }
 
     }
 }
